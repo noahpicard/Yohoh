16560f9f2bcecb9c545ca17ffcef4ac7e61679cb
Noah Picard <noahapicard@gmail.com>
2018-10-13 19:14:07 -0400

    Added Python data

---
+++ PythonDataTest/Assets/PythonDataTest.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PythonDataTest : MonoBehaviour
+{
+	string m_Path;
+
+	System.Diagnostics.Process p;
+
+	int i;
+
+	void Start ()
+	{
+		Screen.fullScreen = !Screen.fullScreen;
+		i = 0;
+		m_Path = Application.dataPath;
+
+		//Output the Game data path to the console
+		//Debug.Log("Path : " + m_Path);
+
+		/*p = new System.Diagnostics.Process();
+		p.StartInfo.FileName = m_Path + "/pythontest.py";
+		p.StartInfo.Arguments = "optional arguments separated with spaces";
+		p.StartInfo.UseShellExecute = false;
+		p.StartInfo.RedirectStandardOutput = true;
+		p.StartInfo.RedirectStandardError = true;
+		p.Start();  */
+
+		Debug.Log ("Start read");
+
+
+		p = new System.Diagnostics.Process();
+		p.StartInfo.RedirectStandardError = true;
+		p.StartInfo.RedirectStandardOutput = true;
+		p.StartInfo.UseShellExecute = false;
+		//p.StartInfo.CreateNoWindow = true;
+		//p.EnableRaisingEvents = true;
+		//p.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+		p.StartInfo.FileName = m_Path + "/webcam-pulse-detector-no_openmdao/get_pulse.py";
+		//p.StartInfo.FileName = m_Path + "/pythontest.py";
+		//p.StartInfo.Arguments = "optional arguments separated with spaces";
+
+		p.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(
+			(s, e) => 
+			{ 
+				Debug.Log("DATA: " + i + " - " + e.Data); 
+				Debug.Log(s);
+			}
+		);
+		p.ErrorDataReceived += new System.Diagnostics.DataReceivedEventHandler(
+			(s, e) => { 
+				Debug.Log("ERROR:" + i + " - " + e.Data); 
+			}
+		);
+
+		p.Start();
+		p.BeginOutputReadLine ();
+	
+		Debug.Log("Read end");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		i = i + 1;
+
+		//Debug.Log (p.StandardOutput.ReadToEnd ());
+
+	}
+}
---
+++ PythonDataTest/Assets/pythontest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python -u
+ 
+# please be sure to chmod +x me otherwise I won't run!
+
+import time
+
+print "Hello, I am the testpython.py python program outputting."
+print
+print "That was a blank line and here is one more."
+print
+print "Here are the arguments you passed in:"
+
+while True:
+  time.sleep(1)
+  print('nice')
